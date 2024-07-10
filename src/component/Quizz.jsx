d9071d26d1dc81eb5f76cbe909fc0d063cd8b6a9
Mostafa Nafady <105189747+Mostafa-Nafady@users.noreply.github.com>
2024-07-10 11:07:28 +0300

    Add control Arrays

--- src/component/Quizz.jsx
+++ src/component/Quizz.jsx
@@ -2,27 +2,95 @@ import { useState } from "react";
 import { Questions } from "./Questions";
 function Quizz() {
   //   const QUESTIONS_ARRAY = ["Ques1", "QUESTION2", "QUestion3"];
-  const [currentQuestion, setCurrentQuestion] = useState(Questions[0].text);
+  const [currentQuestion, setCurrentQuestion] = useState(Questions[0]);
   const [remainQuestions, setRemainQuestions] = useState(Questions);
-  const [answers, setAnswers] = useState("Array of answers ");
+  const [currentAnswers, setCurrentAnswers] = useState({
+    ANSWER: "",
+    checkedIdx: "",
+  });
 
+  const [AnswersArray, setAnswersArray] = useState([]);
+  console.log("Questions", Questions);
+  console.log("currentAnswers", currentAnswers);
+  console.log("AnswersArray", AnswersArray);
   function HandelSubmit() {
     console.log("click");
     /*go to the next Question */
     setRemainQuestions((prev) => {
+      setCurrentAnswers({
+        ANSWER: "",
+        checkedIdx: "",
+      });
       const updatedQuestionsArray = prev.filter(
-        (ques, idx) => ques.text !== currentQuestion
+        (ques, idx) => ques.text !== currentQuestion.text
       );
-      if (prev.length) setCurrentQuestion(updatedQuestionsArray[0].text);
+      if (prev.length) setCurrentQuestion(updatedQuestionsArray[0]);
       return updatedQuestionsArray;
     });
-
-    /*store the Answer and Question*/
+    setAnswersArray((prev) => [
+      ...prev,
+      { quesId: currentQuestion.id, Ans: currentAnswers.ANSWER },
+    ]);
   }
+  function checkHaandler(QuesId, answerIdx) {
+    console.log("checkHaandler");
+    console.log("QuesId &&& answerIdx ", QuesId, answerIdx);
+
+    if (currentAnswers.ANSWER == "") {
+      setCurrentAnswers((prev) => {
+        console.log("currentAnswers", {
+          checkedIdx: answerIdx,
+          ANSWER: currentQuestion.answers[answerIdx],
+        });
+
+        return {
+          checkedIdx: answerIdx,
+          ANSWER: currentQuestion.answers[answerIdx],
+        };
+      });
+      const answersTOuncheck = currentQuestion.answers.filter(
+        (answer, idx) => idx !== answerIdx
+      );
+      console.log("answersTOuncheck", answersTOuncheck);
+    }
+    if (currentAnswers.ANSWER !== "") {
+      setCurrentAnswers({
+        ANSWER: "",
+        checkedIdx: "",
+      });
+    }
+  }
+
+  /*store the Answer and Question*/
+
   return (
     <div>
       {remainQuestions.length ? (
-        <p>{currentQuestion}</p>
+        <div>
+          <p>{currentQuestion.text}</p>
+          <div className="flex">
+            {currentQuestion.answers &&
+              currentQuestion.answers.map((answer, idx) => {
+                return (
+                  <div key={idx}>
+                    <label>{answer}</label>
+                    <input
+                      type="checkbox"
+                      checked={
+                        currentAnswers.checkedIdx === idx &&
+                        currentAnswers.ANSWER
+                      }
+                      onChange={() => checkHaandler(currentQuestion.id, idx)}
+                      disabled={
+                        currentAnswers.checkedIdx !== idx &&
+                        currentAnswers.ANSWER
+                      }
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </div>
       ) : (
         <p>the End Of Ques</p>
       )}
